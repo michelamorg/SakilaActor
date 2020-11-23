1c271fe72ef800f715eea387a9f92ea2aa9187c6
michela.morgillo <michela.morgillo@NAPGLHPO6QCY7FV.dir.svc.accenture.com>
2020-11-23 10:37:23 +0100

    aggiungo classe model

---
+++ SakilaActor/src/main/java/com/myproject/model/Actor.java
@@ -0,0 +1,33 @@
+package com.myproject.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "actor", catalog = "dvdrental", schema = "public")
+
+public class Actor {
+
+	@Id
+	@Column(name = "actor_id")
+	private int actorId;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "last_update")
+	private String lastUpdate;
+
+}
